2ec8e200a6b690a7427e46dcd1a8541bec3dd96c
TmsC21 <t.cupela21@gmail.com>
2023-05-03 15:33:26 +0200

    Commit

--- src/app/Main.java
+++ src/app/Main.java
@@ -3,7 +3,5 @@ package app;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
-        System.out.println("Hello wordl");
-        System.out.println("Hello wordl!!!");
     }
 }
